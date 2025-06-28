e02871d137971ac11d444c66342f4f22bde8be6b
wangjiabao <wangjiabao@joyy.com>
2025-06-28 15:27:58 +0800

    chore: add interface comment

--- core/src/main/java/org/wang/lock/TLock.java
+++ core/src/main/java/org/wang/lock/TLock.java
@@ -11,15 +11,54 @@ public interface TLock {
 
     String getName();
 
+    /**
+     * Attempts to acquire the lock; the thread will block until the lock is acquired.
+     */
     void lock();
 
+    /**
+     * Attempts to acquire the lock; the thread will block until the lock is acquired.
+     * If an interrupt signal is received during the waiting period, an InterruptedException is thrown.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting
+     */
     void lockInterruptibly() throws InterruptedException;
 
+    /**
+     * Releases the lock.
+     */
     void unlock();
 
+    /**
+     * Attempts to acquire the lock and returns the result of the acquisition.
+     *
+     * @param leaseTime Duration for which the lock is held.
+     * @param unit      Time unit for the lease time.
+     * @return true if the lock was acquired, false otherwise.
+     */
     boolean tryLock(long leaseTime, TimeUnit unit);
 
+    /**
+     * Attempts to acquire the lock for a duration of leaseTime.
+     * 1. If successful, returns immediately; if unsuccessful, waits for waitTime.
+     * 2. If the lock is not acquired within waitTime, returns false.
+     *
+     * @param waitTime  Wait time upon failure to acquire the lock.
+     * @param leaseTime Duration for which the lock is held.
+     * @param unit      Time unit for both wait time and lease time.
+     * @return true if the lock was acquired, false otherwise.
+     */
     boolean tryLock(long waitTime, long leaseTime, TimeUnit unit);
 
+    /**
+     * Attempts to acquire the lock for a duration of leaseTime and returns the result of the acquisition.
+     * If an interrupt signal is received during the waiting period, an InterruptedException is thrown.
+     *
+     * @param waitTime  Wait time upon failure to acquire the lock.
+     * @param leaseTime Duration for which the lock is held.
+     * @param unit      Time unit for both wait time and lease time.
+     * @return true if the lock was acquired, false otherwise.
+     * @throws InterruptedException if the thread is interrupted while waiting
+     */
     boolean tryLockInterruptibly(long waitTime, long leaseTime, TimeUnit unit) throws InterruptedException;
 }
